48ab389ce63922d0c4242d6db1fbf7f1ecaab69a
Akmaral <kanatovaakmaral@gmail.com>
2017-08-11 14:19:43 +0600

    removing bootsttrap

--- client/src/components/AdminHome.jsx
+++ client/src/components/AdminHome.jsx
@@ -6,7 +6,7 @@ class AdminHome extends React.Component {
   render() {
     return (
       <div className="container clearfix">
-      <div className="col-md-10 col-md-offset-2 bg-title">
+      <div className=" bg-title">
         <h4>Главная админа</h4>
       </div>
       </div>);
